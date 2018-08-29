d58dbb88429238a73c951d268b3973438a101a91
Jeremy Organ <32566207+Jorgan195@users.noreply.github.com>
2018-08-29 13:37:36 +0100

    DC11.00.01
    Add New field

--- CreateDevEnv.ps1
+++ CreateDevEnv.ps1
@@ -1,13 +1,20 @@
-﻿$mylicense = "c:\temp\mylicense.flf"
-$imageName = "microsoft/dynamics-nav:2017-cu13"
+﻿
+# Updatehosts may require administrator permissions
+
+$mylicense = "c:\Navision\DC Nav 2018 withISV rights.flf"
+$imageName = "microsoft/dynamics-nav:2018"
 $sourceFolder = Join-Path $PSScriptRoot "Source"
 $containerName = Split-Path $PSScriptRoot -Leaf
+
 New-NavContainer -accept_eula `
                  -containerName $containerName `
                  -imageName $imageName `
+                 -memoryLimit 3G `
                  -auth Windows `
                  -licensefile $mylicense `
                  -updateHosts `
                  -includeCSide `
                  -additionalParameters @("--volume ${sourceFolder}:c:\source") 
+
 Import-DeltasToNavContainer -containerName $containerName -deltaFolder $sourceFolder -compile
+
